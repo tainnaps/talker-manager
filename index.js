0ce245555a2280b39085c241cd0fc061cab1ab1d
Taina Santana <tainnaps@gmail.com>
2022-03-18 17:25:02 -0300

    feat: create endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -24,6 +24,20 @@ app.get('/talker', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const fileContent = await fs.readFile('./talker.json', 'utf-8');
+  const talkers = JSON.parse(fileContent);
+  const searchedTalker = talkers.find((talker) => talker.id === parseInt(id, 10));
+
+  if (!searchedTalker) {
+    const message = 'Pessoa palestrante não encontrada';
+    return res.status(404).json({ message });
+  }
+
+  res.status(200).json(searchedTalker);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
