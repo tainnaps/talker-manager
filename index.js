8df800f9666bef725eda999c40ae9b4be740d1bc
Taina Santana <tainnaps@gmail.com>
2022-03-18 18:18:47 -0300

    refactor: adjust talker router require

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs/promises');
+const { talkerRouter } = require('./routers/index');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,30 +13,7 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_req, res) => {
-  const fileContent = await fs.readFile('./talker.json', 'utf-8');
-  const talkers = JSON.parse(fileContent);
-
-  if (talkers.length === 0) {
-    return res.status(200).json([]);
-  }
-
-  res.status(200).json(talkers);
-});
-
-app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-  const fileContent = await fs.readFile('./talker.json', 'utf-8');
-  const talkers = JSON.parse(fileContent);
-  const searchedTalker = talkers.find((talker) => talker.id === parseInt(id, 10));
-
-  if (!searchedTalker) {
-    const message = 'Pessoa palestrante não encontrada';
-    return res.status(404).json({ message });
-  }
-
-  res.status(200).json(searchedTalker);
-});
+app.use('/talker', talkerRouter);
 
 app.listen(PORT, () => {
   console.log('Online');
