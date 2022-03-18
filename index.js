9efdd1f242c5bbc84ac8e7f37f314a42a36b30e3
Taina Santana <tainnaps@gmail.com>
2022-03-18 16:50:33 -0300

    feat: create endpoint GET /talker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,17 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const fileContent = await fs.readFile('./talker.json', 'utf-8');
+  const talkers = JSON.parse(fileContent);
+
+  if (talkers.length === 0) {
+    return res.status(200).json([]);
+  }
+
+  res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
