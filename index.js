740d13442cf682eecceb085fce3cd71b0863cfb9
Taina Santana <tainnaps@gmail.com>
2022-06-19 18:04:44 -0300

    refactor: remove trybe files

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ const { talkerRouter, loginRouter } = require('./routers/index');
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
 
 app.use('/talker', talkerRouter);
 
